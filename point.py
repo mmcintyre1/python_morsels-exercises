32f27c6f4cfeadf72dc3cb2b93e9316259fc7871
unknown <mfmcintyre01@gmail.com>
2020-08-09 15:01:49 -0400

    added dataclass implementation for point

--- point.py
+++ point.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass, astuple
+
+
 class Point:
     def __init__(self, x, y, z):
         self.x = x
@@ -31,17 +34,45 @@ class Point:
             z=self.z * value
         )
 
-    def __rmul__(self, value):
-        return Point(
-            x=value * self.x,
-            y=value * self.y,
-            z=value * self.z
-        )
+    def __rmul__(self, scalar):
+        return self.__mul__(scalar)
 
     def __iter__(self):
         yield self.x
         yield self.y
         yield self.z
+
+
+@dataclass
+class Point:
+
+    """Three-dimensional point."""
+
+    x: float
+    y: float
+    z: float
+
+    def __add__(self, other):
+        """Return copy of our point, shifted by other."""
+        x1, y1, z1 = self
+        x2, y2, z2 = other
+        return Point(x1+x2, y1+y2, z1+z2)
+
+    def __sub__(self, other):
+        """Return copy of our point, shifted by other."""
+        x1, y1, z1 = self
+        x2, y2, z2 = other
+        return Point(x1-x2, y1-y2, z1-z2)
+
+    def __mul__(self, scalar):
+        """Return new copy of our point, scaled by given value."""
+        x, y, z = self
+        return Point(scalar*x, scalar*y, scalar*z)
+
+    __rmul__ = __mul__
+
+    def __iter__(self):
+        yield from astuple(self)
 
 
 if __name__ == '__main__':
