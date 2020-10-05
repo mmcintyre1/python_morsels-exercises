31c0276c3312f5d90d5bbb870374339f27bd76f0
Mike <mfmcintyre01@gmail.com>
2020-10-04 20:39:29 -0400

    cant get lazy evaluation working - peekable might be more complicated than its worth

--- window.py
+++ window.py
@@ -2,31 +2,67 @@ from collections import deque
 
 
 class Peekable:
-    def __init__(self, iterable, cache_size=1):
+    sentinel = object()
+
+    def __init__(self, iterable):
         self._iterable = iter(iterable)
         self._cache = deque()
-        self._cache_size = cache_size
+        self._fill_cache(1)
         self._preview = self._cache[0]
 
-    def _fill_cache(self, n):
-        pass
+    def _fill_cache(self, n=None):
+        if n is None:
+            n = 1
+        while len(self._cache) < n+1:
+            try:
+                next_item = self._iterable.__next__()
+            except StopIteration:
+                next_item = self.sentinel
+            self._cache.append(next_item)
+        self._preview = self._cache[0]
+
+    def __next__(self):
+        if self._cache:
+            result = self._cache.popleft()
+        else:
+            result = self._iterable.__next__()
+        if result is self.sentinel:
+            self.empty = True
+            raise StopIteration
+        return result
+
+    def __iter__(self):
+        return self
 
     def peek(self, stop=1):
-        if stop > len(self._cache):
-            return None
-        return [self._cache[i] for i in range(stop-1)]
+        self._fill_cache(stop)
+        if stop == 1:
+            return [self._cache[0]]
+        else:
+            return [self._cache[i] for i in range(stop)]
+
+    def is_last(self):
+        return self._preview == self.sentinel
 
 
 def window(iterable, n):
+    if n == 0:
+        return []
+
     p = Peekable(iterable)
-    print(list(p))
     for item in p:
-        print(item)
+        lookahead = p.peek(n-1)
+        if lookahead[-1] is p.sentinel:
+            break
+        yield tuple([item] + lookahead)
 
 
 def main():
+    # 1, 4, 9, 16, 25
     numbers = (n**2 for n in [1, 2, 3, 4, 5])
-    print(list(window(numbers, 2)))
+    it = window(numbers, 2)
+    print(next(it))
+    print(next(numbers))
 
 
 if __name__ == '__main__':
