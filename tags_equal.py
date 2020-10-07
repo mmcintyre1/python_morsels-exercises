f7f7877148af689c56e719942c13beaec2f94f87
Mike <mfmcintyre01@gmail.com>
2020-10-06 21:32:05 -0400

    added example of HTML parsing (with no equality)

--- tags_equal.py
+++ tags_equal.py
@@ -1,3 +1,4 @@
+from html.parser import HTMLParser
 import shlex
 
 
@@ -15,5 +16,24 @@ def tags_equal(first_tag, last_tag):
     return split_tag(first_tag) == split_tag(last_tag)
 
 
+class MyHTMLParser(HTMLParser):
+    def error(self, message):
+        pass
+
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+        for attr in attrs:
+            print("     attr:", attr)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+
 if __name__ == '__main__':
-    print(tags_equal("<input value='hello there'>", '<input value="hello there">'))
+    tag_1 = "<input value='hello there' value='ok'>"
+    tag_2 = '<input value="hello there">'
+    p = MyHTMLParser()
+    p.feed(tag_1)
